cc25ee556c38fecf4374d759d8cbd8538e007ffe
Andoni1Barba <Andoni1barba@gmail.com>
2024-09-02 12:04:29 -0400

    svelte

---
+++ spotify-clone/src/components/Greeting.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+
+    let greeting = "Good Morning";
+    if (currentHour >= 12 && currentHour < 18) {
+        greeting = "Good Afternoon";
+    } else if (currentHour >= 18) {
+        greeting = "Good Evening";
+    }
+</script>
+
+<h1 class="text-3xl font-bold">
+    {greeting}, welcome to Spotify Clone!
+</h1>
